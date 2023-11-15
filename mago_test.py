f8dd8eba6c6046bb3836db4496b05ce24f36843a
aswini80 <112609500+aswini80@users.noreply.github.com>
2023-11-15 06:24:03 +0530

    the is my code to push into git

---
+++ mago_test.py
@@ -0,0 +1,16 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://mongo_db:abcd1234@cluster0.eudh50d.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+
+
+dic = {
+    "name": "Radhe",
+    "emailid":"aswinikk@gmail.com",
+    "surname":"Krishna"
+}
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(dic )
